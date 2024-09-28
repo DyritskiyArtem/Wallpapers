63ba87e718ddbcc7e1f18869537d410b3162f47e
DyritskiyArtem <dyritskiyartem2012@gmail.com>
2024-09-28 12:52:20 +0300

    added tag search

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const fs = require("fs");
 const path = require("path");
 
 let photoLinks = require("./photoLinks.json");
+let tags = [];
+
+for (let i = 0; i < photoLinks.length; i++) {
+  const photoLink = photoLinks[i];
+  for (let j = 0; j < photoLink.tag.length; j++) {
+    const tag = photoLink.tag[j];
+
+    if (!tags.includes(tag)) {
+      tags.unshift(tag);
+    }
+  }
+}
+
+tags.sort();
 
 const app = express();
 app.use(express.static("public"));
@@ -22,8 +36,12 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+
 app.get("/", (req, res) => {
   res.setHeader("Content-Type", "text/html; charset=utf-8");
+
+  let {tag} = req.query;
+  console.log(tag);
 
   let html = `
     <html>
@@ -42,44 +60,72 @@ app.get("/", (req, res) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     </head>
     <body>
-        <header>
+        ${tag !== undefined ?
+          `<header class='header2'>
             <h1>Wallpapers Dyritskiy</h1>
-        </header>
+            <nav>
+              <ul>
+                  <li><a href='/'>Back</a></li>
+              </ul>
+            </nav>
+          </header>` :
+            
+          `<header>
+            <h1>Wallpapers Dyritskiy</h1>
+          </header>`  
+        }
 
-        <form method='POST' action='/' enctype='multipart/form-data'>
-            <input name='file' type='file' accept="image/*">
+        <div class='container'>
+          <div>
+            <form method='POST' action='/' enctype='multipart/form-data'>
+                <input name='file' type='file' accept="image/*">
 
-            <div class='divInput'>
-              <label>Name photo</label>          
-              <input name='name' type='text'>  
-              
-              <label>Tag</label>            
-              <input name='tag' type='text'/>
-            </div>
+                <div class='divInput'>
+                  <label>Name photo</label>          
+                  <input name='name' type='text'>  
+                  
+                  <label>Tag</label>            
+                  <input name='tag' type='text'/>
+                </div>
 
-            <button type='submit'>Add photo</button>
-        </form>
-        `;
+                <button type='submit'>Add photo</button>
+            </form>
+          </div>
+
+          <div class='divTag'>
+            <h1>Tag</h1>
+              <div class='aTag'>
+    `;
+
+  for (let i = 0; i < tags.length; i++) {
+    const tag = tags[i];
+    html += `<a href='./?tag=${tag}'>${tag} </a>`;
+  }
+  html += `
+        </div>
+      </div>
+    </div>
+  `;
 
   html += `<div class="gallery">`;
   for (let i = 0; i < photoLinks.length; i++) {
     const photoLink = photoLinks[i];
-
-    html += `
-      <div class="gallery-item">
+    if (tag === undefined || photoLink.tag.includes(tag)) {
+      html += `
+      <div class="gallery-item" id='gallery'>
         <img src="${photoLink.img}" alt="Uploaded image">
         <p>${photoLink.name}</p>
         <p>${photoLink.tag}</p>
         <a href='${photoLink.img}' download>Download</a>
       </div>`
+    }
   }
 
   html += `</div> 
     </body>
     </html>`;
 
-    console.log(__dirname);
-
+  console.log(__dirname);
   res.send(html);
 });
 
@@ -112,21 +158,31 @@ app.post("/", upload.single("file"), (req, res) => {
     `;
   
   if (req.file === undefined) {
-    html += "<div class='container'><p>The file is not specified</p></div>";
+    html += "<div class='container2'><p>The file is not specified</p></div>";
     res.status(400).send(html);
     return;
   }
 
   let { name, tag } = req.body;
 
+  let userTags = tag.split(',');
+  for (let i = 0; i < userTags.length; i++) {
+    let userTag = userTags[i];
+     if (!tags.includes(userTag)) {
+      tags.unshift(userTag);
+    }
+  }
+
+  tags.sort();
+
   if (name.length < 3 || name.length > 25) {
-    html += "<div class='container'><p>Name must be between 3 and 25 characters long.</p></div>";
+    html += "<div class='container2'><p>Name must be between 3 and 25 characters long.</p></div>";
     res.status(400).send(html);
     return;
   }
 
   html += `
-      <div class='container'><p>Your image has been uploaded</p></div>
+      <div class='container2'><p>Your image has been uploaded</p></div>
     </div> 
   </body>
   </html>`;
